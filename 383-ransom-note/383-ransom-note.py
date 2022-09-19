149d2acf30a76dc7c30be5d8125f04f30ba4bbeb
Ricardo Santos <ricardo.ams@hotmail.com>
2022-09-19 18:53:33 +0100

    Time: 190 ms (8.61%), Space: 14.2 MB (52.99%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        chars={}
+        for m in magazine:
+            if m not in chars.keys():
+                chars[m]=0
+            chars[m]+=1
+            
+        for r in ransomNote:
+            try:
+                chars[r]-=1
+            except:
+                return False
+            if chars[r]< 0:
+                return False
+        
+        return True
