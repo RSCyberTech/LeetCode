af2b3665c9ddd5c7b20f7a0acb08bec57919fddc
Ricardo Santos <ricardo.ams@hotmail.com>
2023-01-02 22:07:13 +0000

    Time: 1677 ms, Memory: 45.7 MB - LeetHub

---
+++ palindrome-linked-list/palindrome-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        items=[]
+        while True:
+            try:
+                items.append(head.val)
+                head=head.next
+            except:
+                break
+        if len(items) < 2:
+            return True
+        if len(items)%2 == 0:
+            firstHalf=items[:int(len(items)/2)]
+            secondHalfReversed=list(reversed(items[int(len(items)/2):]))
+        else:
+            firstHalf=items[:int(len(items)/2)]
+            secondHalfReversed=list(reversed(items[int(len(items)/2)+1:]))
+            print(len(items))
+        return firstHalf==secondHalfReversed
